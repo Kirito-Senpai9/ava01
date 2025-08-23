d60e1a28454ff81ec61c18eda5faeaf2e6e99864
Eduardo Kaic <eduardokaic928@gmail.com>
2025-08-22 23:04:17 -0300

    Teste

--- app-estoque/src/api.js
+++ app-estoque/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Atenção: substitua pelo IP da sua máquina na mesma rede do celular/Expo Go.
 // Ex.: http://192.168.0.10:3000
-export const API_BASE_URL = 'http://10.0.0.190:3000';
+export const API_BASE_URL = 'http://192.168.0.101:3000';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
